628498cd300ed5e512bcc1d8cbf912e01be2dc97
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-25 10:20:54 +0000

    Docu for the EKS module

--- lib/Signer/AWSv4/EKS.pm
+++ lib/Signer/AWSv4/EKS.pm
@@ -62,3 +62,61 @@ package Signer::AWSv4::EKS;
   });
 
 1;
+### main pod documentation begin ###
+
+=encoding UTF-8
+
+=head1 NAME
+
+Signer::AWSv4::EKS - Generate tokens for logging in to EKS Kubernetes clusters
+
+=head1 SYNOPSIS
+
+  use Signer::AWSv4::EKS;
+  my $signer = Signer::AWSv4::EKS->new(
+    access_key => 'AKIAIOSFODNN7EXAMPLE',
+    secret_key => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+    cluster_id => 'eks_cluster_name',
+  );
+  my $token = $signer->signed_qstring;
+
+=head1 DESCRIPTION
+
+Generate tokens for logging in to EKS Kubernetes clusters. This implements the same algorithm that the Heptio autheticator does. L<https://github.com/kubernetes-sigs/aws-iam-authenticator>
+
+=head1 Request Attributes
+
+This module adds one required attributee in the constructor for obtaining a token
+
+=head2 cluster_id String
+
+The name of the EKS cluster in AWS
+
+=head1 Signature Attributes
+
+=head2 token
+
+The authentication token to be passed to the Kubernetes cluster (via Authorization header or kubectl --token)
+
+=head2 k8s_json
+
+This wraps the token in the appropiate JSON output for using the token as kubectl pluggable 
+authentication module
+
+=head1 SEE ALSO
+
+L<https://github.com/kubernetes-sigs/aws-iam-authenticator>
+
+=head1 BUGS and SOURCE
+
+The source code is located here: L<https://github.com/pplu/AWSv4Signer>
+
+Please report bugs to: L<https://github.com/pplu/AWSv4Signer/issues>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2018 by CAPSiDE
+
+This code is distributed under the Apache 2 License. The full text of the license can be found in the LICENSE file included with this module.
+
+=cut
